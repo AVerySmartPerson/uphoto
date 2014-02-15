e7483b1fb25d1cc4dcfa9f661f6dfba8b96b604b
root <root@parallels-Parallels-Virtual-Platform.(none)>
2014-02-15 10:52:05 +1000

    Added a Javascript folder and enabled the upload form and upload processing script to support multiple image file uploads

--- upload_form.php
+++ upload_form.php
@@ -1,9 +1,10 @@
 <?php
 /**
-* Displays a form that allows users to upload an image file with a maximum size of 20Mb.
+* Displays a form that allows users to upload multiple image files, each with a 
+* maximum size of 20Mb.
 *
 * @author Mark Johnman
-* @date 7/2/2014
+* @date 15/2/2014
 */
 ?>
 
@@ -11,11 +12,21 @@
 <html>
    <head>
       <title> Upload an Image </title>
+      <script src="js/upload_form_functions"></script>
    </head>
    <body>
       <h1> Upload an Image</h1>
-      <form enctype="multipart/form-data" action="upload_processing.php" method="post">
-          Choose an image to upload (maximum file size is 20Mb): <input name="image" type="file">
+      <form id="image_upload_form" enctype="multipart/form-data" action="upload_processing.php" method="post">
+          <button type="button" onclick="addFile(this.value)" value="images">Add Image</button>
+          <br>
+          <br>
+          <!--The ID of the div below needs to be the same as the value of the Add Image button in order for the JS to work properly. -->
+          <div id="images">
+            <div id="file1">
+               Choose an image to upload (maximum file size is 20Mb): <input name="image1" type="file">
+            </div>
+          </div>
+          <br>
           <input type="submit" value="Upload Image">
       </form>
    </body>
--- upload_processing.php
+++ upload_processing.php
@@ -1,20 +1,16 @@
 <?php
 /**
-* Validates an image uploaded via a HTML form. If the image
-* is valid, it is given a unique name and placed in the 
-* images folder. If the image/upload isn't valid, an error
-* message is displayed. 
+* Validates one or multiple images uploaded via a HTML form. 
+* If an image is valid, it is given a unique name, placed 
+* in the images folder and a success message is displayed. 
+* If an image/upload isn't valid, an error message is 
+* displayed. At the conclusion of the process, a message
+* listing the total number of images uploaded is displayed.
 *
 * @author Mark Johnman
-* @date 7/2/2014
+* @date 15/2/2014
 */
 
-if (!isset($_FILES["image"])) {
-  exit("You didn't upload an image between 0Mb and the maximum size");
-}
-
-// Add current time so that the image is guaranteed to have a unique name
-$image_destination = "images/" . time() . "-" . basename($_FILES["image"]["name"]);
 $error_messages = array(
    1 => "Your image is larger than the maximum size", 
    2 => "Your image is larger than the maximum size", 
@@ -22,12 +18,32 @@ $error_messages = array(
    4 => "You didn't upload an image"
 );
 
-if ($_FILES["image"]["error"] != 0) {
-   exit($error_messages[$_FILES["image"]["error"]]);
-} else if (!getimagesize($_FILES["image"]["tmp_name"])) {
-   exit("Your upload failed because the file you attempted to upload is not an image");
-} else if (move_uploaded_file($_FILES["image"]["tmp_name"], $image_destination)) {
-   exit("Your image has been successfully uploaded");
-} else {
-   exit("Your upload has failed");
+$number_images_uploaded = 0;
+
+foreach ($_FILES as $file) {
+   // Add current time so that the image is guaranteed to have a unique name
+   $image_destination = "images/" . time() . "-" . basename($file["name"]);
+   
+   /* 
+   * Stops an error message being displayed for every blank file input element. This could be annoying
+   * if the user created 10 file input elements and then only uploaded two images. If the user didn't 
+   * upload any files at all, this will be reflected by the total number of images uploaded message at 
+   * the end of the script.
+   */ 
+   if ($file["error"] == 4) {
+      continue;
+   } else if ($file["error"] != 0) {
+      echo $file["name"]. ": " . $error_messages[$file["error"]];
+   } else if (!getimagesize($file["tmp_name"])) {
+      echo $file["name"]. ": " . "Your upload failed because the file you attempted to upload is not an image";
+   } else if (move_uploaded_file($file["tmp_name"], $image_destination)) {
+      echo $file["name"]. ": " . "Your image has been successfully uploaded";
+      $number_images_uploaded++;
+   } else {
+      echo $file["name"]. ": " . "Your upload has failed";
+   }
+
+   echo "<br>";
 }
+
+echo ($number_images_uploaded == 1) ? "1 image was uploaded" : "$number_images_uploaded images were uploaded";
