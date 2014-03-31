b5a0a743a1594731ea1bc35a17d3a9649bbe6ca7
root <root@parallels-Parallels-Virtual-Platform.(none)>
2014-04-01 01:32:11 +1000

    Improved overall design and layout for final lab tasks

--- index.php
+++ index.php
@@ -6,8 +6,11 @@
 * thumbnails are displayed for a certain period of time. This ensures
 * continuity accross all web browsers and users of the web appplication.
 *
+* Also displays a form that allows users to upload multiple image files, 
+* each with a maximum size of 100Mb, and a blurb about Uphoto.
+*
 * @author Mark Johnman
-* @date 15/2/2014
+* @date 1/4/2014
 */
 
 // Number of minutes that the same randomly chosen thumbnails should be displayed.
@@ -21,29 +24,127 @@ $num_columns = 5;
 $image_ids = array();
 
 foreach (glob("images/*.{png,jpeg,jpg,gif}", GLOB_BRACE) as $filename) {
-   /*
-   * Ensures that images uploaded within the current display block aren't included in the list
-   * of potential thumbnails (if the list changed, different thumbnails would be displayed).
-   */
-   if (time() - filemtime($filename) > (60 * $num_mins_to_display)) {
-      array_push($image_ids, preg_replace("/images\/(.*)\.(png|jpeg|jpg|gif)/", "$1", $filename));
-   }
+	/*
+	* Ensures that images uploaded within the current display block aren't included in the list
+	* of potential thumbnails (if the list changed, different thumbnails would be displayed).
+	*/
+	if (time() - filemtime($filename) > (60 * $num_mins_to_display)) {
+		array_push($image_ids, preg_replace("/images\/(.*)\.(png|jpeg|jpg|gif)/", "$1", $filename));
+	}
 }
 
 // Seeds the random number generator so that the same thumbnails are chosen for the specified time period.
 $seed = time() / (60 * $num_mins_to_display);
 mt_srand($seed);
 
-for ($row = 1; $row <= $num_rows; $row++) {
-   for ($column = 1; $column <= $num_columns; $column++) {
-      $index = mt_rand(0, count($image_ids) - 1);
-      $image_id = $image_ids[$index];
+?>
 
-      // Removes the image ID just chosen from the list of image IDs so that the same image can't be displayed twice.
-      array_splice($image_ids, $index, 1);
+<!DOCTYPE html>
+<html lang="en">
+	<head>  
+		<meta name="author" content="Mark Johnman">
+		<meta name="application-name" content="Uphoto Imgur Clone">
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
-      echo "<a href='view.php?id=$image_id'><img src='thumbnail.php?id=$image_id'></a>";
-      echo "&nbsp;";
-   }
-   echo "<br><br>";
-}
+		<title> Welcome to Uphoto </title>
+
+		<link rel="stylesheet" href="/lib/bootstrap/css/bootstrap.min.css">  
+
+		<style>
+			body {
+				background-color: black;
+				text-align: center;
+			}
+			.pull-center {
+				margin: 0 auto;
+				display: block;
+				text-align: center;
+			}
+		</style>    
+
+		<script src="js/upload_form_functions"></script>
+		<script src="/lib/jquery/jquery-1.10.2.min.js"></script>
+		<script src="/lib/bootstrap/js/bootstrap.min.js"></script>      
+	</head>
+	<body>
+		<br>
+		<div class="row">
+			<div class="col-md-8">
+				<?php
+				for ($row = 1; $row <= $num_rows; $row++) {
+					echo "<div class='col-xs-3'>";
+					for ($column = 1; $column <= $num_columns; $column++) {
+						$index = mt_rand(0, count($image_ids) - 1);
+						$image_id = $image_ids[$index];
+
+						// Removes the image ID just chosen from the list of image IDs so that the same image can't be displayed twice.
+						array_splice($image_ids, $index, 1);
+
+						echo "<a data-toggle='modal' href='#myModal' class='thumbnail'><img src='thumbnail.php?id=$image_id'></a>";
+						echo "&nbsp;";
+					?>
+					
+					<!-- Modal -->
+					<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+					  <div class="modal-dialog">
+						<div class="modal-content">
+						  <div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+							<h4 class="modal-title" id="myModalLabel">View Image</h4>
+						  </div>
+						  <div class="modal-body">
+							<img src="view.php?id=<?php echo $image_id; ?>">
+						  </div>
+						  <div class="modal-footer">
+							<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+						  </div>
+						</div>
+					  </div>
+					</div>
+
+					<?php
+
+					}
+					echo "</div>";
+				}
+				?>
+			</div>
+
+			<div class="col-md-4">
+				<div class="well well-lg">
+					<h1>Upload Images</h1>
+					<form id="image_upload_form" enctype="multipart/form-data" action="upload_processing.php" method="post" role="form">
+						<!--The ID of the div below needs to be the same as the value of the Add Image button in 
+							order for the JS to work properly. -->	
+						<br>								
+						<button type="button" onclick="addFile(this.value)" value="images" class="btn btn-default">Add Image</button>
+						<br>
+						<br>
+						<br>
+						<div id="images">
+							<div id="file1">
+								<input type="file" name="file1" class="pull-center">
+								<br>
+							</div>
+						</div>
+						<br>
+						<input type="submit" value="Upload Image/s" class="btn btn-lg btn-primary">
+					</form>
+				</div>
+				<div class="well well-lg">
+					<h1>About Uphoto</h1>
+					<br>
+					<p>
+						Uphoto is a site where you can upload images and view them. Above is a form that allows users to upload multiple image files, each with a maximum size of 100Mb.
+						<br><br>
+						On the left is a grid of randomly chosen thumbnails of some of the images stored by the web application. Each thumbnail is a link to a view of the full-sized image. The grid of thumbnails is set up so that the same thumbnails are displayed for a certain period of time.
+					</p>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
+
+
+
--- upload_processing.php
+++ upload_processing.php
@@ -7,8 +7,12 @@
 * displayed. At the conclusion of the process, a message
 * listing the total number of images uploaded is displayed.
 *
+* The upload report is displayed in a modal window. When the
+* window is closed, the user is redirected back to the index
+* page.
+*
 * @author Mark Johnman
-* @date 15/2/2014
+* @date 1/4/2014
 */
 
 // Error messages corresponding with PHP file upload error codes.
@@ -21,31 +25,86 @@ $error_messages = array(
 
 // Total number of images successfully uploaded.
 $number_images_uploaded = 0;
+?>
 
-foreach ($_FILES as $file) {
-   // Add current time so that the image is guaranteed to have a unique name
-   $image_destination = "images/" . time() . "-" . basename($file["name"]);
-   
-   /* 
-   * Stops an error message being displayed for every blank file input element. This could be annoying
-   * if the user created 10 file input elements and then only uploaded two images. If the user didn't 
-   * upload any files at all, this will be reflected by the total number of images uploaded message at 
-   * the end of the script.
-   */ 
-   if ($file["error"] == 4) {
-      continue;
-   } else if ($file["error"] != 0) {
-      echo $file["name"]. ": " . $error_messages[$file["error"]];
-   } else if (!getimagesize($file["tmp_name"])) {
-      echo $file["name"]. ": " . "Your upload failed because the file you attempted to upload is not an image";
-   } else if (move_uploaded_file($file["tmp_name"], $image_destination)) {
-      echo $file["name"]. ": " . "Your image has been successfully uploaded";
-      $number_images_uploaded++;
-   } else {
-      echo $file["name"]. ": " . "Your upload has failed";
-   }
+<!DOCTYPE html>
+<html lang="en">
+	<head>  
+		<meta name="author" content="Mark Johnman">
+		<meta name="application-name" content="Uphoto Imgur Clone">
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
-   echo "<br>";
-}
+		<title> Welcome to Uphoto </title>
 
-echo ($number_images_uploaded == 1) ? "1 image was uploaded" : "$number_images_uploaded images were uploaded";
+		<link rel="stylesheet" href="/lib/bootstrap/css/bootstrap.min.css">  
+
+		<style>
+			body {
+				background-color: black;
+				text-align: center;
+			}
+			.pull-center {
+				margin: 0 auto;
+				display: block;
+				text-align: center;
+			}
+			.modal.in {
+   				display:block;
+			}
+		</style>    
+
+		<script src="js/upload_form_functions"></script>
+		<script src="/lib/jquery/jquery-1.10.2.min.js"></script>
+		<script src="/lib/bootstrap/js/bootstrap.min.js"></script>      
+	</head>
+	<body>
+
+	<!-- Modal -->
+	<div class="modal fade in" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<a href="index.php"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button></a>
+					<h4 class="modal-title" id="myModalLabel">Upload Report</h4>
+				</div>
+				<div class="modal-body">
+					<?php
+					foreach ($_FILES as $file) {
+						// Add current time so that the image is guaranteed to have a unique name
+						$image_destination = "images/" . time() . "-" . basename($file["name"]);
+
+						/* 
+						* Stops an error message being displayed for every blank file input element. This could be annoying
+						* if the user created 10 file input elements and then only uploaded two images. If the user didn't 
+						* upload any files at all, this will be reflected by the total number of images uploaded message at 
+						* the end of the script.
+						*/ 
+						if ($file["error"] == 4) {
+							continue;
+						} else if ($file["error"] != 0) {
+							echo $file["name"]. ": " . $error_messages[$file["error"]];
+						} else if (!getimagesize($file["tmp_name"])) {
+						  	echo $file["name"]. ": " . "Your upload failed because the file you attempted to upload is not an image";
+						} else if (move_uploaded_file($file["tmp_name"], $image_destination)) {
+						  	echo $file["name"]. ": " . "Your image has been successfully uploaded";
+						  	$number_images_uploaded++;
+						} else {
+						  	echo $file["name"]. ": " . "Your upload has failed";
+						}
+
+						echo "<br><br>";
+					}
+
+					echo ($number_images_uploaded == 1) ? "1 image was uploaded." : "$number_images_uploaded images were uploaded.";
+					?>
+				</div>
+				<div class="modal-footer">
+					<a href="index.php"><button type="button" class="btn btn-primary" data-dismiss="modal">Close</button></a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	</body>
+</html>
